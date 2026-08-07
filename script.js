5859c0de6f6989cc6fe2f0a9e57fc713e27b41f6
Nguyễn Đức Phát <nguyenducphat2246@gmail.com>
2026-08-07 23:42:51 +0700

    Thay đổi cách tải map (dùng buffer) để tải map nhanh hơn tránh đợi lâu

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ var map = L.map('map', {
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
+    keepBuffer: 6,
+    updateWhenIdle: false,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
